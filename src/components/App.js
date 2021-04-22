dd71483857ec369072c316904138f742ee8d7e98
Ian Marshall <imarshall14@gmail.com>
2021-04-22 17:23:42 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,37 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }));
+  };
+
+  onChangeType = ({target: {value}}) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: value
+      }
+    });
+  }
+
+  onFindPetsClick = () => {
+    this.fetchPets();
+  }
+
+  onAdoptPet = () => {
+    this.setState({
+      isAdopted: true
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +55,11 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onFindPetsClick={this.onFindPetsClick}/>
+              {/* ^the lefthand side of the = defines the name of the prop on the lower level */}
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
+            {this.gender}
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.name}
           </a>
           <div className="meta">
             <span className="date">PET TYPE</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.age}</p>
+            <p>Weight: {this.weight}</p>
           </div>
         </div>
         <div className="extra content">
